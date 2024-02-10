6ec6ff118fbc04307189e4cc90012eb478981b55
cloudbound2 <robert.zacharski@data-minds.com>
2024-02-09 23:23:46 -0500

    mod my-react-app App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello, React App on port 3000. HEAD -&gt; main, origin/main OK?</h1>
+      <h1>Hello, React App on port 3000. 
+          HEAD -&gt; main, origin/main OK.</h1>
     </div>
   );
 }
